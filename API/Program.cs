a4fcc977653bac7ae2f997e0879d75c7c1e50422
koknq <koknq1990@gmail.com>
2024-10-16 17:15:35 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,12 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
